29dd78e642d58ccc99deee81c9fc477579f3f927
richie-AI <79308498+richie-AI@users.noreply.github.com>
2021-09-16 10:49:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ response = requests.get(url="https://api.openweathermap.org/data/2.5/onecall", p
 response.raise_for_status()
 weather_data = response.json()
 
-weather_slice = weather_data["hourly"][:20]
+weather_slice = weather_data["hourly"][:12]
 will_rain = False
 for hour_data in weather_slice:
     weather_condition_code = hour_data["weather"][0]["id"]
